393dbe8f69bd32f830c002baf9e187cc9723fff7
Kent vejrup Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-01-24 19:26:55 +0100

    working on URLS

--- src/Network/URI/Link.php
+++ src/Network/URI/Link.php
@@ -19,7 +19,168 @@
         {
 
         }
-
-
-    }
+		
+		// Variables
+		private ?string $scheme = null;
+		
+		private ?string $host = null;
+		
+		private ?int $port = null;
+	
+		private ?string $user = null;
+		
+		private ?string $pass = null;
+	
+		private ?string $path = null;
+		
+		private ?string $query = null;
+		
+		private ?string $fragment = null;
+	
+	
+		/**
+		 * @return string|null
+		 */
+		public function getFragment(): ?string
+		{
+			return $this->fragment;
+		}
+	
+		/**
+		 * @return string|null
+		 */
+		public function getHost(): ?string
+		{
+			return $this->host;
+		}
+	
+		/**
+		 * @return string|null
+		 */
+		public function getPass(): ?string
+		{
+			return $this->pass;
+		}
+	
+		/**
+		 * @return string|null
+		 */
+		public function getPath(): ?string
+		{
+			return $this->path;
+		}
+	
+		/**
+		 * @return int|null
+		 */
+		public function getPort(): ?int
+		{
+			return $this->port;
+		}
+	
+		/**
+		 * @return string|null
+		 */
+		public function getQuery(): ?string
+		{
+			return $this->query;
+		}
+	
+		/**
+		 * @return string|null
+		 */
+		public function getScheme(): ?string
+		{
+			return $this->scheme;
+		}
+	
+	
+		/**
+		 * @return string|null
+		 */
+		public function getUser(): ?string
+		{
+			return $this->user;
+		}
+	
+		/**
+		 * @param string|null $fragment
+		 */
+		public function setFragment(
+			?string $fragment
+		): void
+		{
+			$this->fragment = $fragment;
+		}
+	
+		/**
+		 * @param string|null $host
+		 */
+		public function setHost(
+			?string $host
+		): void
+		{
+			$this->host = $host;
+		}
+	
+		/**
+		 * @param string|null $pass
+		 */
+		public function setPass(
+			?string $pass
+		): void
+		{
+			$this->pass = $pass;
+		}
+	
+		/**
+		 * @param string|null $path
+		 */
+		public function setPath(
+			?string $path
+		): void
+		{
+			$this->path = $path;
+		}
+	
+		/**
+		 * @param int|null $port
+		 */
+		public function setPort(
+			?int $port
+		): void
+		{
+			$this->port = $port;
+		}
+	
+		/**
+		 * @param string|null $query
+		 */
+		public function setQuery(
+			?string $query
+		): void
+		{
+			$this->query = $query;
+		}
+	
+		/**
+		 * @param string|null $scheme
+		 */
+		public function setScheme(
+			?string $scheme
+		): void
+		{
+			$this->scheme = $scheme;
+		}
+	
+		/**
+		 * @param string|null $user
+		 */
+		public function setUser(
+			?string $user
+		): void
+		{
+			$this->user = $user;
+		}
+	}
 ?>
---
+++ src/Network/URI/LinkScheme.php
@@ -0,0 +1,30 @@
+<?php
+	namespace IoJaegers\Hrbf\Network\URI;
+
+	enum LinkScheme
+	{
+		case HTTP;
+		
+		case HTTPS;
+		
+		case FTP;
+		
+		case PHP;
+		
+		case ZLIB;
+		
+		case DATA;
+		
+		case GLOB;
+		
+		case PHAR;
+		
+		case SSH2;
+		
+		case RAR;
+		
+		case OGG;
+		
+		case EXPECT;
+	}
+?>
